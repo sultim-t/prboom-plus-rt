f3570760b2d4cecf2b7c0a5fdd2ebfe1d8414f30
proff_fs <proff_fs@localhost>
2001-06-17 17:47:37 +0000

    fixed D_abs again

--- prboom2/src/m_fixed.h
+++ prboom2/src/m_fixed.h
@@ -1,7 +1,7 @@
 /* Emacs style mode select   -*- C++ -*- 
  *-----------------------------------------------------------------------------
  *
- * $Id: m_fixed.h,v 1.8 2001/06/13 17:17:29 proff_fs Exp $
+ * $Id: m_fixed.h,v 1.9 2001/06/17 17:47:37 proff_fs Exp $
  *
  *  PrBoom a Doom port merged with LxDoom and LSDLDoom
  *  based on BOOM, a modified and improved DOOM engine
@@ -52,8 +52,8 @@ typedef int fixed_t;
  * killough 9/05/98: better code seems to be gotten from using inlined C
  */
 
-#ifdef I386_ASM
-# ifdef _MSC_VER
+#ifdef _MSC_VER
+# ifdef I386_ASM
 #pragma warning( disable : 4035 )
 __inline static int D_abs(int x)
 {
@@ -65,17 +65,17 @@ __inline static int D_abs(int x)
         sub eax,edx
     }
 }
-# else /* _MSC_VER */
-#define D_abs(x) ({fixed_t _t = (x), _s = _t >> (8*sizeof _t-1); (_t^_s)-_s;})
-# endif /* _MSC_VER */
-#else /* I386_ASM */
-inline static const D_abs(x)
+# else /* I386_ASM */
+inline static const int D_abs(x)
 {
 	fixed_t _t = (x),_s;
 	_s = _t >> (8*sizeof _t-1);
 	return (_t^_s)-_s;
 }
-#endif /* I386_ASM */
+# endif /* I386_ASM */
+#else /* _MSC_VER */
+#define D_abs(x) ({fixed_t _t = (x), _s = _t >> (8*sizeof _t-1); (_t^_s)-_s;})
+#endif /* _MSC_VER */
 
 /*
  * Fixed Point Multiplication
