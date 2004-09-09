4c7d439bd38f77663ca0b92ebead197fe68ef371
cph <cph@localhost>
2004-09-09 19:02:56 +0000

    Start level code also uses P_Map* and must be wrapped (else P_MapStart catches leftovers when in MBF or Boom compat. mode).

--- prboom2/src/p_setup.c
+++ prboom2/src/p_setup.c
@@ -1453,6 +1453,7 @@ void P_SetupLevel(int episode, int map, int playermask, skill_t skill)
 
   bodyqueslot = 0;
   deathmatch_p = deathmatchstarts;
+  P_MapStart();
   P_LoadThings(lumpnum+ML_THINGS);
 
   // if deathmatch, randomly spawn the active players
@@ -1473,6 +1474,8 @@ void P_SetupLevel(int episode, int map, int playermask, skill_t skill)
 
   // set up world state
   P_SpawnSpecials();
+
+  P_MapEnd();
 
   // preload graphics
   if (precache)
