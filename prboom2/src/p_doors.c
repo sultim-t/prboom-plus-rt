855b89d9881e04e86a5e682000ff5cd4e5fd05f6
cph <cph@localhost>
2006-04-01 16:11:58 +0000

    comp_doorlight should work at Boom compat. Take e6y's code from prboom-plus to emulate Boom's one-step light change, but use MBF's smooth changing at mbf_compatibility and above.

--- prboom2/src/p_doors.c
+++ prboom2/src/p_doors.c
@@ -127,7 +127,10 @@ void T_VerticalDoor (vldoor_t* door)
             );
 
       /* killough 10/98: implement gradual lighting effects */
-      if (door->lighttag && door->topheight - door->sector->floorheight)
+      // e6y: "Tagged doors don't trigger special lighting" handled wrong
+      // http://sourceforge.net/tracker/index.php?func=detail&aid=1411400&group_id=148658&atid=772943
+      // Old code: if (door->lighttag && door->topheight - door->sector->floorheight)
+      if (door->lighttag && door->topheight - door->sector->floorheight && compatibility_level >= mbf_compatibility)
         EV_LightTurnOnPartway(door->line,
                               FixedDiv(door->sector->ceilingheight -
                                        door->sector->floorheight,
@@ -174,6 +177,10 @@ void T_VerticalDoor (vldoor_t* door)
           default:
             break;
         }
+        // e6y: "Tagged doors don't trigger special lighting" handled wrong
+        // http://sourceforge.net/tracker/index.php?func=detail&aid=1411400&group_id=148658&atid=772943
+        if (door->lighttag && door->topheight - door->sector->floorheight && compatibility_level < mbf_compatibility)
+          EV_LightTurnOnPartway(door->line,0);
       }
       /* jff 1/31/98 turn lighting off in tagged sectors of manual doors
        * killough 10/98: replaced with gradual lighting code
@@ -217,7 +224,10 @@ void T_VerticalDoor (vldoor_t* door)
             );
 
       /* killough 10/98: implement gradual lighting effects */
-      if (door->lighttag && door->topheight - door->sector->floorheight)
+      // e6y: "Tagged doors don't trigger special lighting" handled wrong
+      // http://sourceforge.net/tracker/index.php?func=detail&aid=1411400&group_id=148658&atid=772943
+      // Old code: if (door->lighttag && door->topheight - door->sector->floorheight)
+      if (door->lighttag && door->topheight - door->sector->floorheight && compatibility_level >= mbf_compatibility)
         EV_LightTurnOnPartway(door->line,
                               FixedDiv(door->sector->ceilingheight -
                                        door->sector->floorheight,
@@ -254,6 +264,10 @@ void T_VerticalDoor (vldoor_t* door)
 
         /* jff 1/31/98 turn lighting on in tagged sectors of manual doors
    * killough 10/98: replaced with gradual lighting code */
+        // e6y: "Tagged doors don't trigger special lighting" handled wrong
+        // http://sourceforge.net/tracker/index.php?func=detail&aid=1411400&group_id=148658&atid=772943
+        if (door->lighttag && door->topheight - door->sector->floorheight && compatibility_level < mbf_compatibility)
+          EV_LightTurnOnPartway(door->line,FRACUNIT);
       }
       break;
   }
