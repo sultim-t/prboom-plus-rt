3fcfd14244b2ac0a25fb486b72f9e562adf9c18a
cph <cph@localhost>
2006-04-11 18:41:07 +0000

    Fix compile error, m_fixed.h not known here. D_abs is really for fixed_t anyway, so let's use abs.

--- prboom2/src/SDL/i_joy.c
+++ prboom2/src/SDL/i_joy.c
@@ -33,6 +33,8 @@
 #ifndef lint
 #endif /* lint */
 
+#include <stdlib.h>
+
 #include "SDL.h"
 #include "doomdef.h"
 #include "doomtype.h"
@@ -72,10 +74,10 @@ void I_PollJoystick(void)
     (SDL_JoystickGetButton(joystick, 2)<<2) |
     (SDL_JoystickGetButton(joystick, 3)<<3);
   axis_value = SDL_JoystickGetAxis(joystick, 0) / 3000;
-  if (D_abs(axis_value)<10) axis_value=0;
+  if (abs(axis_value)<10) axis_value=0;
   ev.data2 = axis_value;
   axis_value = SDL_JoystickGetAxis(joystick, 1) / 3000;
-  if (D_abs(axis_value)<10) axis_value=0;
+  if (abs(axis_value)<10) axis_value=0;
   ev.data3 = axis_value;
 
   D_PostEvent(&ev);
