716da6a1b050be76ec0d2564113c9451ad680f46
proff_fs <proff_fs@localhost>
2006-07-25 15:54:35 +0000

    For some reason I can't find out why the 'reset colors' ansi code doesn't work in the Mac launcher, so just set fore and background to the default explicitely, that works everywhere.

--- prboom2/src/i_main.c
+++ prboom2/src/i_main.c
@@ -252,7 +252,7 @@ static void I_EndDoom(void)
           /* reset everything when we start a new line */
           oldbg = -1;
           oldcolor = -1;
-          printf("\e[0m\n");
+          printf("\e[39m\e[49m\n");
         }
         /* foreground color */
         bold = 0;
@@ -297,7 +297,7 @@ static void I_EndDoom(void)
   }
 #ifndef _WIN32
   if (endoom_mode & endoom_colours)
-    puts("\e[0m"); /* cph - reset colours */
+    puts("\e[39m\e[49m"); /* cph - reset colours */
   PrintVer();
 #else /* _WIN32 */
   I_uSleep(3000000); // CPhipps - don't thrash cpu in this loop
