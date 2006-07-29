c90f2030d6b886583d8418f58e6b58e066632b9e
neil <neil@localhost>
2006-07-29 19:02:00 +0000

    Licenses for some Mac files

--- prboom2/src/MAC/LauncherApp.h
+++ prboom2/src/MAC/LauncherApp.h
@@ -1,3 +1,5 @@
+// This file is hereby placed in the Public Domain -- Neil Stevens
+
 #import <Cocoa/Cocoa.h>
 
 @interface LauncherApp : NSObject
--- prboom2/src/MAC/LauncherApp.m
+++ prboom2/src/MAC/LauncherApp.m
@@ -1,3 +1,5 @@
+// This file is hereby placed in the Public Domain -- Neil Stevens
+
 #import "LauncherApp.h"
 
 #import <Foundation/NSArray.h>
