158e31a9266892404a30a782656b0d18291e0a9f
proff_fs <proff_fs@localhost>
2006-07-29 16:17:14 +0000

    Clear player mobj's at level end - they're allocated PU_LEVEL so we free them in the next tic. This was causing consistency failures in netgames. From trunk r733.

--- prboom2/src/g_game.c
+++ prboom2/src/g_game.c
@@ -966,7 +966,7 @@ static void G_PlayerFinishLevel(int player)
   player_t *p = &players[player];
   memset(p->powers, 0, sizeof p->powers);
   memset(p->cards, 0, sizeof p->cards);
-  p->mo->flags &= ~MF_SHADOW;   // cancel invisibility
+  p->mo = NULL;           // cph - this is allocated PU_LEVEL so it's gone
   p->extralight = 0;      // cancel gun flashes
   p->fixedcolormap = 0;   // cancel ir gogles
   p->damagecount = 0;     // no palette changes
