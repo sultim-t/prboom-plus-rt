48658af89f0437f77a065dfc5c8690f59404356c
proff_fs <proff_fs@localhost>
2006-08-09 01:15:29 +0000

    Fixed endian issues.

--- prboom2/src/st_lib.c
+++ prboom2/src/st_lib.c
@@ -100,8 +100,8 @@ static void STlib_drawNum
   int   numdigits = n->width;
   int   num = *n->num;
 
-  int   w = SHORT(n->p[0].width);
-  int   h = SHORT(n->p[0].height);
+  int   w = n->p[0].width;
+  int   h = n->p[0].height;
   int   x = n->x;
 
   int   neg;
@@ -286,10 +286,10 @@ void STlib_updateMultIcon
   {
     if (mi->oldinum != -1)
     {
-      x = mi->x - SHORT(mi->p[mi->oldinum].leftoffset);
-      y = mi->y - SHORT(mi->p[mi->oldinum].topoffset);
-      w = SHORT(mi->p[mi->oldinum].width);
-      h = SHORT(mi->p[mi->oldinum].height);
+      x = mi->x - mi->p[mi->oldinum].leftoffset;
+      y = mi->y - mi->p[mi->oldinum].topoffset;
+      w = mi->p[mi->oldinum].width;
+      h = mi->p[mi->oldinum].height;
 
 #ifdef RANGECHECK
       if (y - ST_Y < 0)
@@ -354,10 +354,10 @@ void STlib_updateBinIcon
 
   if (*bi->on && (bi->oldval != *bi->val || refresh))
   {
-    x = bi->x - SHORT(bi->p->leftoffset);
-    y = bi->y - SHORT(bi->p->topoffset);
-    w = SHORT(bi->p->width);
-    h = SHORT(bi->p->height);
+    x = bi->x - bi->p->leftoffset;
+    y = bi->y - bi->p->topoffset;
+    w = bi->p->width;
+    h = bi->p->height;
 
 #ifdef RANGECHECK
     if (y - ST_Y < 0)
