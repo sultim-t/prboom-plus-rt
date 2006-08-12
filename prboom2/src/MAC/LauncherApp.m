a2fecf96c639a5754a63db55b7e6dd8dd1ab5655
entryway <entryway@localhost>
2006-08-12 16:44:13 +0000

    Merged r2010:2015 from trunk.

--- prboom2/src/MAC/LauncherApp.m
+++ prboom2/src/MAC/LauncherApp.m
@@ -64,7 +64,10 @@ static LauncherApp *LApp;
 		if([[defaults objectForKey:@"Demo Drawer State"] boolValue])
 			[demoDrawer open];
 		if([[defaults objectForKey:@"Console State"] boolValue])
+		{
 			[consoleController showWindow:self];
+			[window makeKeyAndOrderFront:self];
+		}
 
 		[[configFileButtonController field] setObjectValue:[defaults objectForKey:@"Config File"]];
 	}
