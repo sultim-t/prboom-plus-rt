83e667d110071f83dfeb90703eed13d3fb9be8b4
entryway <entryway@localhost>
2006-08-26 09:20:12 +0000

    fix compilation problems

--- prboom2/src/m_menu.c
+++ prboom2/src/m_menu.c
@@ -4702,8 +4702,10 @@ boolean M_Responder (event_t* ev) {
         ptr1->action();
       
       //e6y
+#ifdef GL_DOOM
       if ((ptr1->m_flags&S_CANT_GL_ARB_MULTITEXTURE) && !gl_arb_multitexture)
         warn_about_changes(ptr1->m_flags & S_CANT_GL_ARB_MULTITEXTURE);
+#endif
     }
     M_SelectDone(ptr1);                           // phares 4/17/98
     return true;
