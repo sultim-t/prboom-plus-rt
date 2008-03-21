8cb09a30798cfaa37482bf53ba02d63e95b711dd
entryway <entryway@localhost>
2008-03-21 13:58:34 +0000

    fix for linear filtering for sprites

--- prboom2/src/r_things.c
+++ prboom2/src/r_things.c
@@ -296,8 +296,14 @@ static vissprite_t *R_NewVisSprite(void)
 {
   if (num_vissprite >= num_vissprite_alloc)             // killough
     {
+      size_t num_vissprite_alloc_prev = num_vissprite_alloc;
+
       num_vissprite_alloc = num_vissprite_alloc ? num_vissprite_alloc*2 : 128;
       vissprites = realloc(vissprites,num_vissprite_alloc*sizeof(*vissprites));
+      
+      //e6y: set all fields to zero
+      memset(vissprites + num_vissprite_alloc_prev, 0,
+        (num_vissprite_alloc - num_vissprite_alloc_prev)*sizeof(*vissprites));
     }
  return vissprites + num_vissprite++;
 }
@@ -428,11 +434,6 @@ static void R_DrawVisSprite(vissprite_t *vis, int x1, int x2)
     {
       texturecolumn = frac>>FRACBITS;
       dcvars.texu = frac;
-
-#ifdef RANGECHECK
-      if (texturecolumn < 0 || texturecolumn >= patch->width)
-        I_Error ("R_DrawSpriteRange: Bad texturecolumn");
-#endif
 
       R_DrawMaskedColumn(
         patch,
