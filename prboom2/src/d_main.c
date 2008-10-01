4de83df5d203eb5ccf318b86a893cd420df8fbaa
entryway <entryway@localhost>
2008-10-01 17:18:02 +0000

    Fixed old bug in searching responsefile.

--- prboom2/src/d_main.c
+++ prboom2/src/d_main.c
@@ -942,7 +942,7 @@ static void FindResponseFile (void)
         // proff 04/05/2000: Added for searching responsefile
         if (size < 0)
         {
-          strcat(strcpy(fname,I_DoomExeDir()),&myargv[i][1]);
+          SNPRINTF(fname, sizeof(fname), "%s/%s", I_DoomExeDir(), &myargv[i][1]);
           AddDefaultExtension(fname,".rsp");
 	  size = M_ReadFile(fname, &file);
         }
