749489f53f4f8f8abba1bce578cd7348fa249dc9
proff_fs <proff_fs@localhost>
2008-10-12 18:28:26 +0000

    Discontinued experiment.

--- pyboom/pyboom/data/patches/__init__.py
+++
@@ -1,1 +0,0 @@
-from simple import Patches
--- pyboom/pyboom/wads/__init__.py
+++
@@ -1,6 +0,0 @@
-import adapters
-from multisourcewad import MultiSourceWad
-from mmapwad import WadFileMMap
-
-def open_wadfile(filename):
-    return WadFileMMap(filename)
