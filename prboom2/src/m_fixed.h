2444769fa9f97c5f7449230b804fbc738e2f68a2
entryway <entryway@localhost>
2008-10-16 23:29:12 +0000

    added comment for the previous revision r2892 to know what happened

--- prboom2/src/m_fixed.h
+++ prboom2/src/m_fixed.h
@@ -142,7 +142,12 @@ inline static CONSTFUNC fixed_t FixedMul(fixed_t a, fixed_t b)
 #pragma warning( disable : 4035 )
 __inline static fixed_t FixedDiv(fixed_t a, fixed_t b)
 {
-    //e6y: zg is a master of engineer science
+    // e6y
+    // zg is a master of engineer science.
+    // Fixed crash with FixedDiv(-2147483648,x)
+    // Some other ports (Eternity, Chocolate) return wrong value instead of MAXINT.
+    // For example FixedDiv(-2147483648,-30576) should return INT_MAX instead of 307907126
+    // 307907126 is truncated correct int64 value: 4602874423 - 2^32 = 307907126
     if ((unsigned)D_abs(a) >> 14 >= (unsigned)D_abs(b))
         return (a^b)<0 ? INT_MIN : INT_MAX;
     __asm
