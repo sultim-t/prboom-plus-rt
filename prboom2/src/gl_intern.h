ec2fed302561f6bcb544e3f2ae6cb22cd9aa8d3a
entryway <entryway@localhost>
2009-02-19 13:10:45 +0000

    missed constant

--- prboom2/src/gl_intern.h
+++ prboom2/src/gl_intern.h
@@ -73,7 +73,8 @@ typedef enum
   GLTEXTURE_HASHOLES  = 0x00000004,
   GLTEXTURE_SKY       = 0x00000008,
   GLTEXTURE_HIRES     = 0x00000010,
-  GLTEXTURE_CLAMPY    = 0x00000020,
+  GLTEXTURE_HASNOHIRES= 0x00000020,
+  GLTEXTURE_CLAMPY    = 0x00000040,
 } GLTexture_flag_t;
 
 typedef struct gl_strip_coords_s
