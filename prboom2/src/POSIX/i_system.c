6e5efe2bb19db667411fe443f441fb76f6d3b2f9
entryway <entryway@localhost>
2009-11-06 11:45:04 +0000

    Missed include?

--- prboom2/src/POSIX/i_system.c
+++ prboom2/src/POSIX/i_system.c
@@ -33,6 +33,10 @@
  *-----------------------------------------------------------------------------
  */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include <stdio.h>
 
 #include <stdarg.h>
