3478ecf74dc5c90b806ad4ca336a654728a9c6f6
entryway <entryway@localhost>
2010-04-14 22:20:20 +0000

    Fixed the latest known incompatibility (MBF)
    PrBoom does not freeze with "help_monsters 1" as a bonus.

--- prboom2/src/p_inter.c
+++ prboom2/src/p_inter.c
@@ -652,6 +652,9 @@ static void P_KillMobj(mobj_t *source, mobj_t *target)
   target->flags |= MF_CORPSE|MF_DROPOFF;
   target->height >>= 2;
 
+  // killough 8/29/98: remove from threaded list
+  P_UpdateThinker(&target->thinker);
+
   if (!((target->flags ^ MF_COUNTKILL) & (MF_FRIEND | MF_COUNTKILL)))
     totallive--;
 
