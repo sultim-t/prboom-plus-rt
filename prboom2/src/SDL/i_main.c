6372e7293ec8059141960b81c33cfca83340939b
rjy <rjy@localhost>
2010-12-10 12:28:42 +0000

    endoom: import rboom's UTF-8 console endoom renderer
    Tested on a linux virtual console and urxvt. UTF-8 byte sequences work
    much better than raw codepage 437 characters on modern terminals.
    
    - codepage 437 to unicode conversion table, derived from [1]
    
    - unicode code point to utf-8 byte sequence conversion, derived from [2]
      (possible future work: use libc wide character functions instead of
      assuming UTF-8 and doing the bit manipulation yourself. Requires
      __STDC_ISO_10646__ defined - i.e. a wchar_t is a unicode code point)
    
    - treat endoom data as unsigned so a colour byte having its top bit set
      (the blink flag) does not confuse the conversion to ansi colour into
      producing black-on-black text. Also stop trying to access it through a
      two-byte array, to avoid warnings [3]
    
    - some issues with bold text resolved: the old code conflated bold and
      foreground colour into a single state, giving unnecessary churn. The
      messy convert() function is replaced by a simple array lookup.
      Similarly, do not conflate the background colour and the blink flag.
    
    - also bold off was done as a full reset and a confusing hack to force
      a background colour change to compensate, when there is an escape
      sequence that turns bold off and does nothing else
    
    - actually implement the blink flag. Who am I to deny wadmakers yet
      another way to make players' eyes bleed? Depends on your terminal
      support, of course (urxvt treats it as "make the background bold")
    
    [1] http://www.unicode.org/Public/MAPPINGS/VENDORS/MICSFT/PC/CP437.TXT
        http://www.unicode.org/Public/MAPPINGS/VENDORS/MISC/IBMGRAPH.TXT
    
    [2] http://groups.google.com/group/rec.games.roguelike.nethack/msg/df523225e1d22a77
    
    [3] http://groups.google.com/group/comp.lang.c/msg/afd6e0f3bf4f4d26

--- prboom2/src/SDL/i_main.c
+++ prboom2/src/SDL/i_main.c
@@ -152,12 +152,215 @@ static void PrintVer(void)
   lprintf(LO_INFO,"%s\n",I_GetVersionString(vbuf,200));
 }
 
+static unsigned int cp437_to_unicode(unsigned char cp437)
+{
+  static const unsigned short cp437_to_unicode_table[256] =
+  {
+    0x0020, 0x263a, 0x263b, 0x2665, 0x2666, 0x2663, 0x2660, 0x2022,
+    0x25d8, 0x25cb, 0x25d9, 0x2642, 0x2640, 0x266a, 0x266b, 0x263c,
+    0x25ba, 0x25c4, 0x2195, 0x203c, 0x00b6, 0x00a7, 0x25ac, 0x21a8,
+    0x2191, 0x2193, 0x2192, 0x2190, 0x221f, 0x2194, 0x25b2, 0x25bc,
+    0x0020, 0x0021, 0x0022, 0x0023, 0x0024, 0x0025, 0x0026, 0x0027,
+    0x0028, 0x0029, 0x002a, 0x002b, 0x002c, 0x002d, 0x002e, 0x002f,
+    0x0030, 0x0031, 0x0032, 0x0033, 0x0034, 0x0035, 0x0036, 0x0037,
+    0x0038, 0x0039, 0x003a, 0x003b, 0x003c, 0x003d, 0x003e, 0x003f,
+    0x0040, 0x0041, 0x0042, 0x0043, 0x0044, 0x0045, 0x0046, 0x0047,
+    0x0048, 0x0049, 0x004a, 0x004b, 0x004c, 0x004d, 0x004e, 0x004f,
+    0x0050, 0x0051, 0x0052, 0x0053, 0x0054, 0x0055, 0x0056, 0x0057,
+    0x0058, 0x0059, 0x005a, 0x005b, 0x005c, 0x005d, 0x005e, 0x005f,
+    0x0060, 0x0061, 0x0062, 0x0063, 0x0064, 0x0065, 0x0066, 0x0067,
+    0x0068, 0x0069, 0x006a, 0x006b, 0x006c, 0x006d, 0x006e, 0x006f,
+    0x0070, 0x0071, 0x0072, 0x0073, 0x0074, 0x0075, 0x0076, 0x0077,
+    0x0078, 0x0079, 0x007a, 0x007b, 0x007c, 0x007d, 0x007e, 0x2302,
+    0x00c7, 0x00fc, 0x00e9, 0x00e2, 0x00e4, 0x00e0, 0x00e5, 0x00e7,
+    0x00ea, 0x00eb, 0x00e8, 0x00ef, 0x00ee, 0x00ec, 0x00c4, 0x00c5,
+    0x00c9, 0x00e6, 0x00c6, 0x00f4, 0x00f6, 0x00f2, 0x00fb, 0x00f9,
+    0x00ff, 0x00d6, 0x00dc, 0x00a2, 0x00a3, 0x00a5, 0x20a7, 0x0192,
+    0x00e1, 0x00ed, 0x00f3, 0x00fa, 0x00f1, 0x00d1, 0x00aa, 0x00ba,
+    0x00bf, 0x2310, 0x00ac, 0x00bd, 0x00bc, 0x00a1, 0x00ab, 0x00bb,
+    0x2591, 0x2592, 0x2593, 0x2502, 0x2524, 0x2561, 0x2562, 0x2556,
+    0x2555, 0x2563, 0x2551, 0x2557, 0x255d, 0x255c, 0x255b, 0x2510,
+    0x2514, 0x2534, 0x252c, 0x251c, 0x2500, 0x253c, 0x255e, 0x255f,
+    0x255a, 0x2554, 0x2569, 0x2566, 0x2560, 0x2550, 0x256c, 0x2567,
+    0x2568, 0x2564, 0x2565, 0x2559, 0x2558, 0x2552, 0x2553, 0x256b,
+    0x256a, 0x2518, 0x250c, 0x2588, 0x2584, 0x258c, 0x2590, 0x2580,
+    0x03b1, 0x00df, 0x0393, 0x03c0, 0x03a3, 0x03c3, 0x00b5, 0x03c4,
+    0x03a6, 0x0398, 0x03a9, 0x03b4, 0x221e, 0x03c6, 0x03b5, 0x2229,
+    0x2261, 0x00b1, 0x2265, 0x2264, 0x2320, 0x2321, 0x00f7, 0x2248,
+    0x00b0, 0x2219, 0x00b7, 0x221a, 0x207f, 0x00b2, 0x25a0, 0x00a0,
+  };
+  return cp437_to_unicode_table[cp437];
+}
+
+static void unicode_to_utf8(unsigned char *utf8, unsigned int unicode)
+{
+  if (unicode < 0x80) {
+    *utf8++ = unicode;
+  } else if (unicode < 0x800) {
+    *utf8++ = 0xC0 | ((unicode>>6) & 0x1F);
+    *utf8++ = 0x80 | ( unicode     & 0x3F);
+  } else if (unicode < 0x10000) { // only need basic plane for cp437
+    *utf8++ = 0xE0 | ((unicode>>12) & 0x1F);
+    *utf8++ = 0x80 | ((unicode>> 6) & 0x3F);
+    *utf8++ = 0x80 | ( unicode      & 0x3F);
+  }
+  *utf8 = '\0';
+}
+
+static int endoom_selectlump(void)
+{
+  int lump_eb, lump_ed, lump = -1;
+
+  /* CPhipps - ENDOOM/ENDBOOM selection */
+  lump_eb = W_CheckNumForName("ENDBOOM");/* jff 4/1/98 sign our work    */
+  lump_ed = W_CheckNumForName("ENDOOM"); /* CPhipps - also maybe ENDOOM */
+
+  if (lump_eb == -1)
+    lump = lump_ed;
+  else if (lump_ed == -1)
+    lump = lump_eb;
+  else
+  { /* Both ENDOOM and ENDBOOM are present */
+#define LUMP_IS_NEW(num) (!((lumpinfo[num].source == source_iwad) || \
+                            (lumpinfo[num].source == source_auto_load)))
+    switch ((LUMP_IS_NEW(lump_ed) ? 1 : 0) |
+            (LUMP_IS_NEW(lump_eb) ? 2 : 0)) {
+    case 1:
+      lump = lump_ed;
+      break;
+    case 2:
+      lump = lump_eb;
+      break;
+    default:
+      /* Both lumps have equal priority, both present */
+      lump = (P_Random(pr_misc) & 1) ? lump_ed : lump_eb;
+      break;
+    }
+  }
+
+  return lump;
+}
+
+struct endoom_state
+{
+  int bg;
+  int color;
+  int bold;
+  int blink;
+};
+
+static void endoom_init(struct endoom_state *state)
+{
+  state->bg = state->color = state->bold = state->blink = -1;
+}
+
+static void endoom_start(struct endoom_state *state)
+{
+  printf("\e[0m\n");
+}
+
+static void endoom_charstyle(struct endoom_state *state, unsigned char style)
+{
+  static unsigned char cga_to_ansi[] = { 0, 4, 2, 6, 1, 5, 3, 7 }; // convert()
+  static unsigned char bold_to_ansi[] = { 22, 1 }; // intensity normal/high
+  static unsigned char blink_to_ansi[] = { 25, 5 }; // blinking off/on
+
+  // style is a bitfield, treat it as such
+  int color = (style     ) & 0x7;
+  int bold  = (style >> 3) & 0x1;
+  int bg    = (style >> 4) & 0x7;
+  int blink = (style >> 7) & 0x1;
+
+  /* foreground color */
+  if (color != state->color)
+  {
+    state->color = color;
+    /* we buffer everything or output is horrendously slow */
+    printf("\e[%dm", 30 + cga_to_ansi[color]);
+  }
+
+  // bold, now handled separately from foreground
+  if (bold != state->bold)
+  {
+    state->bold = bold;
+    printf("\e[%dm", bold_to_ansi[bold]);
+  }
+
+  /* background color */
+  if (bg != state->bg)
+  {
+    state->bg = bg;
+    printf("\e[%dm", 40 + cga_to_ansi[bg]);
+  }
+
+  // blink flag, if your terminal supports it
+  if (blink != state->blink)
+  {
+    state->blink = blink;
+    printf("\e[%dm", blink_to_ansi[blink]);
+  }
+}
+
+static void endoom_printchar(unsigned char cp437)
+{
+  unsigned int unicode;
+  unsigned char utf8[8];
+
+  unicode = cp437_to_unicode(cp437);
+
+  unicode_to_utf8(utf8, unicode);
+
+  printf("%s", utf8);
+}
+
+static void endoom_endofline(struct endoom_state *state)
+{
+  // reset background colour at new line to avoid "spillage"
+  state->bg = -1;
+  printf("\e[49m\n");
+}
+
+static void endoom_finish(struct endoom_state *state)
+{
+  printf("\e[0m"); /* cph - reset colours */
+}
+
 /* I_EndDoom
  * Prints out ENDOOM or ENDBOOM, using some common sense to decide which.
  * cphipps - moved to l_main.c, made static
  */
 static void I_EndDoom(void)
 {
+  int lump = endoom_selectlump();
+
+  if (lump != -1)
+  {
+    const unsigned char *endoom = W_CacheLumpNum(lump);
+    int i, l = W_LumpLength(lump) / 2;
+    int j, n;
+
+    /* cph - colour ENDOOM by rain */
+    struct endoom_state state;
+    endoom_init(&state);
+
+    endoom_start(&state);
+
+    for (j=0, n=80; j<l; j+=80, n+=80)
+    {
+      for (i=j; i<l && i<n; i++)
+      {
+        endoom_charstyle(&state, endoom[2*i+1]);
+        endoom_printchar(endoom[2*i+0]);
+      }
+      endoom_endofline(&state);
+    }
+
+    endoom_finish(&state);
+
+    W_UnlockLumpNum(lump);
+  }
+
+  PrintVer();
 }
 
 
