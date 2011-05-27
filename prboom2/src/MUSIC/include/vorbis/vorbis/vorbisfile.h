e255229f4c6641102f22f967f13d374d334dc439
entryway <entryway@localhost>
2011-05-27 18:50:38 +0000

    _fseeki64() is replaced with fseek() for VC6

--- prboom2/src/MUSIC/include/vorbis/vorbis/vorbisfile.h
+++ prboom2/src/MUSIC/include/vorbis/vorbis/vorbisfile.h
@@ -56,7 +56,15 @@ static int _ov_header_fseek_wrap(FILE *f,ogg_int64_t off,int whence){
 #ifdef __MINGW32__
   return fseeko64(f,off,whence);
 #elif defined (_WIN32)
-  return _fseeki64(f,off,whence);
+  #ifdef _MSC_VER
+    #if _MSC_VER >= 1300
+      return _fseeki64(f,off,whence);
+    #else
+      return fseek(f,(long)off,whence);
+    #endif
+  #else
+    return _fseeki64(f,off,whence);
+  #endif
 #else
   return fseek(f,off,whence);
 #endif
