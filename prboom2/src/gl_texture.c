9aa6d43d122d1d58cc1ea7f3ddc81bef367f1912
entryway <entryway@localhost>
2012-02-29 15:50:54 +0000

    do not crash in gld_precache after glboom-plus -warp 1 -nodraw -nosound -devparm

--- prboom2/src/gl_texture.c
+++ prboom2/src/gl_texture.c
@@ -1354,7 +1354,7 @@ void gld_Precache(void)
   int usehires = (gl_texture_external_hires) || 
     (gl_texture_internal_hires && r_have_internal_hires);
 
-  if (doSkip)
+  if (doSkip || nodrawers)
     return;
 
   if (!usehires)
