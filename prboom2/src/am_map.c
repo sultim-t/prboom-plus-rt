67a45c73ae6338db6dd0591345bf1056ff08e12b
entryway <entryway@localhost>
2012-03-21 08:51:44 +0000

    missed updating of m_x2/y2

--- prboom2/src/am_map.c
+++ prboom2/src/am_map.c
@@ -308,12 +308,8 @@ static void AM_SetFPointFloatValue(fpoint_t *p)
 //
 static void AM_activateNewScale(void)
 {
-  m_x += m_w/2;
-  m_y += m_h/2;
   m_w = FTOM(f_w);
   m_h = FTOM(f_h);
-  m_x -= m_w/2;
-  m_y -= m_h/2;
   m_x2 = m_x + m_w;
   m_y2 = m_y + m_h;
 }
@@ -2284,6 +2280,8 @@ void AM_Drawer (void)
   {
     m_x = (viewx >> FRACTOMAPBITS) - m_w/2;
     m_y = (viewy >> FRACTOMAPBITS) - m_h/2;
+    m_x2 = m_x + m_w;
+    m_y2 = m_y + m_h;
   }
 
   {
