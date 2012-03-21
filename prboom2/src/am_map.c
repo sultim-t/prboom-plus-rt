2ffa878ee04c05f8741516b12d8bdb2aa9d8505b
entryway <entryway@localhost>
2012-03-21 10:23:26 +0000

    oops

--- prboom2/src/am_map.c
+++ prboom2/src/am_map.c
@@ -308,8 +308,12 @@ static void AM_SetFPointFloatValue(fpoint_t *p)
 //
 static void AM_activateNewScale(void)
 {
+  m_x += m_w/2;
+  m_y += m_h/2;
   m_w = FTOM(f_w);
   m_h = FTOM(f_h);
+  m_x -= m_w/2;
+  m_y -= m_h/2;
   m_x2 = m_x + m_w;
   m_y2 = m_y + m_h;
 }
