b048df10dea5722433b3729ed9281c99296de6a6
entryway <entryway@localhost>
2013-06-25 07:28:38 +0000

    fix R_InitSkyMap for widescreen resolutions

--- prboom2/src/r_sky.c
+++ prboom2/src/r_sky.c
@@ -58,51 +58,66 @@ fixed_t freelookviewheight;
 //
 void R_InitSkyMap(void)
 {
-  int skyheight;
-
-  if (!textureheight)
-    return;
-
-	// There are various combinations for sky rendering depending on how tall the sky is:
-	//        h <  128: Unstretched and tiled, centered on horizon
-	// 128 <= h <  200: Can possibly be stretched. When unstretched, the baseline is
-	//                  28 rows below the horizon so that the top of the texture
-	//                  aligns with the top of the screen when looking straight ahead.
-	//                  When stretched, it is scaled to 228 pixels with the baseline
-	//                  in the same location as an unstretched 128-tall sky, so the top
-	//					of the texture aligns with the top of the screen when looking
-	//                  fully up.
-	//        h == 200: Unstretched, baseline is on horizon, and top is at the top of
-	//                  the screen when looking fully up.
-	//        h >  200: Unstretched, but the baseline is shifted down so that the top
-	//                  of the texture is at the top of the screen when looking fully up.
-  
-  skyheight = textureheight[skytexture]>>FRACBITS;
-  skystretch = false;
-  skytexturemid = 0;
-  if (skyheight >= 128 && skyheight < 200)
+  if (!GetMouseLook())
   {
-    skystretch = (r_stretchsky && skyheight >= 128 && GetMouseLook());
-    skytexturemid = -28*FRACUNIT;
-  }
-  else if (skyheight > 200)
-  {
-    skytexturemid = (200 - skyheight) << FRACBITS;
-  }
-
-  if (viewwidth != 0 && viewheight != 0)
-  {
-    skyiscale = 200 * FRACUNIT / freelookviewheight;
-    skyiscale = (fixed_t)((int_64_t)skyiscale * FieldOfView / 2048);
-  }
-
-  if (skystretch)
-  {
-    skyiscale = (fixed_t)((int_64_t)skyiscale * skyheight / SKYSTRETCH_HEIGHT);
-    skytexturemid = (int)((int_64_t)skytexturemid * skyheight / SKYSTRETCH_HEIGHT);
+    skystretch = false;
+    skytexturemid = 100*FRACUNIT;
+    if (viewwidth != 0)
+    {
+      skyiscale = (fixed_t)(((uint_64_t)FRACUNIT * SCREENWIDTH * 200) / (viewwidth * SCREENHEIGHT));
+    }
   }
   else
   {
-    skytexturemid = 100*FRACUNIT;
+    int skyheight;
+
+    if (!textureheight)
+      return;
+
+    // There are various combinations for sky rendering depending on how tall the sky is:
+    //        h <  128: Unstretched and tiled, centered on horizon
+    // 128 <= h <  200: Can possibly be stretched. When unstretched, the baseline is
+    //                  28 rows below the horizon so that the top of the texture
+    //                  aligns with the top of the screen when looking straight ahead.
+    //                  When stretched, it is scaled to 228 pixels with the baseline
+    //                  in the same location as an unstretched 128-tall sky, so the top
+    //					of the texture aligns with the top of the screen when looking
+    //                  fully up.
+    //        h == 200: Unstretched, baseline is on horizon, and top is at the top of
+    //                  the screen when looking fully up.
+    //        h >  200: Unstretched, but the baseline is shifted down so that the top
+    //                  of the texture is at the top of the screen when looking fully up.
+
+    skyheight = textureheight[skytexture]>>FRACBITS;
+    skystretch = false;
+    skytexturemid = 0;
+    if (skyheight >= 128 && skyheight < 200)
+    {
+      skystretch = (r_stretchsky && skyheight >= 128);
+      skytexturemid = -28*FRACUNIT;
+    }
+    else if (skyheight > 200)
+    {
+      skytexturemid = (200 - skyheight) << FRACBITS;
+    }
+
+    if (viewwidth != 0 && viewheight != 0)
+    {
+      //skyiscale = 200 * FRACUNIT / freelookviewheight;
+      skyiscale = (fixed_t)(((uint_64_t)FRACUNIT * SCREENWIDTH * 200) / (viewwidth * SCREENHEIGHT));
+      // line below is from zdoom, but it works incorrectly with prboom
+      // with widescreen resolutions (eg 1280x720) by some reasons
+      //skyiscale = (fixed_t)((int_64_t)skyiscale * FieldOfView / 2048);
+    }
+
+    if (skystretch)
+    {
+      skyiscale = (fixed_t)((int_64_t)skyiscale * skyheight / SKYSTRETCH_HEIGHT);
+      skytexturemid = (int)((int_64_t)skytexturemid * skyheight / SKYSTRETCH_HEIGHT);
+    }
+    else
+    {
+      skytexturemid = 100*FRACUNIT;
+    }
   }
 }
