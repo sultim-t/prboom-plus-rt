sultim-t/prboom-plus-rt

commit e0d4316111b0c9a04f0e22f640a55d1ff4821c49

Bug fix : clear buffer on updating sound, can cause noise

prboom2/src/SDL/i_sound.c
@@ -448,6 +448,8 @@ static void I_UpdateSound(void *unused, Uint8 *stream, int len)
   // Mixing channel index.
   int       chan;
 
+  memset(stream,0,len);
+
   // NSM: when dumping sound, ignore the callback calls and only
   // service dumping calls
   if (dumping_sound && unused != (void *) 0xdeadbeef)

commit c4b64448a07fbab06087a516b02327975ee8a125

Temporary fix for SDL_Audio output stream buffer clear bug (#83)
* Temporary fix for SDL player bug.

Commit e0d4316111b0c9a04f0e22f640a55d1ff4821c49 introduced a bug
where the SDL MIDI player would be silent. A temporary fix has
been applied onto that bug, where the commit's fix is skipped if
the MIDI player selected is currently the SDL one.

Please note that this is only supposed to be a temporary fix, and
a placeholder for a more definitive one. Also, other MIDI players
have not been tested yet.

* Added .vscode to root gitignore

* Fixed the SDL MIDI player check.

It seems SDL MIDI means that `snd_midiplayer` is set to
`NULL`. I will keep that in mind next time.

* Undone formatting and gitignore changes

* Reverted more slight formatting-related changes

Those few had also slipped by.

Please note for posterity that functions copied to new files
(in gus-experimental branch, which is not PR-ready yet) due
to being static will not have the same formatting as the
original ones. The original ones will be left untouched, though.

This is not relevant to the gus-dev branch (more closely PRBoom-specific,
with a focus on fixing bugs and helping implementing actually useful
features, rather than experimental ones).

prboom2/src/SDL/i_sound.c
@@ -448,6 +448,9 @@ static void I_UpdateSound(void *unused, Uint8 *stream, int len)
   // Mixing channel index.
   int       chan;
 
+  if (snd_midiplayer == NULL) // This is but a temporary fix. Please do remove after a more definitive one!
+    memset(stream, 0, len);
+
   // NSM: when dumping sound, ignore the callback calls and only
   // service dumping calls
   if (dumping_sound && unused != (void *) 0xdeadbeef)