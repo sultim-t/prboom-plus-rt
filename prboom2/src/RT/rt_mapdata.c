d4c4dc3344de2d222fa1fc4bdc2e7b2b6383f883
Sultim Tsyrendashiev <sultim.tsyrendashiev@gmail.com>
2022-03-19 22:26:59 +0000

    - added license notice

--- prboom2/src/RT/rt_mapdata.c
+++ prboom2/src/RT/rt_mapdata.c
@@ -24,7 +24,6 @@
  *---------------------------------------------------------------------
  */
 
-#pragma once
+#include "rt_main.h"
 
-void RT_TryApplyHUDCustomScale(enum patch_translation_e flags, float *p_xpos, float *p_ypos, float *p_width, float *p_height);
-const char *RT_GetFlashlightHintString(int key_flashlight);
+static float *md_sectorids;
